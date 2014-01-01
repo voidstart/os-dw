79d15601d54ad748f62f6eea15be41528bd5f4db
Wang Wei <voidstarting@gmail.com>
2014-01-01 19:24:27 +0800

    revert back to old setting

--- boot.asm
+++ boot.asm
@@ -25,12 +25,12 @@ bpbBytesPerSector:    DW 512
 bpbSectorsPerCluster:   DB 1
 bpbReservedSectors:   DW 1
 bpbNumberOfFATs:      DB 2
-bpbRootEntries:       DW 512
+bpbRootEntries:       DW 224
 bpbTotalSectors:      DW 2880
 bpbMedia:               DB 0xF0
 bpbSectorsPerFAT:       DW 9
-bpbSectorsPerTrack:   DW 32
-bpbHeadsPerCylinder:  DW 16
+bpbSectorsPerTrack:   DW 18
+bpbHeadsPerCylinder:  DW 2
 bpbHiddenSectors:       DD 0
 bpbTotalSectorsBig:     DD 0
 bsDriveNumber:          DB 0
