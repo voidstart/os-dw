e7787e1fc2b71193d5159312348ad65f8bd86cce
Wang Wei <voidstarting@gmail.com>
2014-01-08 07:20:42 +0800

    update

--- mount-update-stage23.sh
+++ mount-update-stage23.sh
@@ -1,3 +1,4 @@
 hdiutil mount floppy.img
 cp -f ./STAGE2.SYS /Volumes/UNTITLED
+cp -f ./KRNL.SYS /Volumes/UNTITLED
 hdiutil unmount /Volumes/UNTITLED
--- nasm-assemble.sh
+++ nasm-assemble.sh
@@ -1,2 +1,3 @@
 nasm -f bin boot.asm -o boot.bin
 nasm -f bin stage2.asm -o STAGE2.SYS
+nasm -f bin stage3.asm -o KRNL.SYS
