8179d97a176f0c64b6c5fe5d790d6611aca7ffcd
Wang Wei <voidstarting@gmail.com>
2014-01-07 10:41:54 +0800

    update

--- stage2.asm
+++ stage2.asm
@@ -22,13 +22,13 @@ jmp	main				; go to start
 
 %include "stdio.inc"			; basic i/o routines
 %include "Gdt.inc"			; Gdt routines
-%include "A20.inc"
+%include "A20.inc"			; A20 enabling
 
 ;*******************************************************
 ;	Data Section
 ;*******************************************************
 
-LoadingMsg db "Preparing to load operating system...", 0x0D, 0x0A, 0x00
+LoadingMsg db 0x0D, 0x0A, "Searching for Operating System...", 0x00
 
 ;*******************************************************
 ;	STAGE 2 ENTRY POINT
@@ -55,13 +55,6 @@ main:
 	sti				; enable interrupts
 
 	;-------------------------------;
-	;   Print loading message	;
-	;-------------------------------;
-
-	mov	si, LoadingMsg
-	call	Puts16
-
-	;-------------------------------;
 	;   Install our GDT		;
 	;-------------------------------;
 
@@ -74,15 +67,24 @@ main:
 	call	EnableA20_KKbrd_Out
 
 	;-------------------------------;
+	;   Print loading message	;
+	;-------------------------------;
+
+	mov	si, LoadingMsg
+	call	Puts16
+
+	;-------------------------------;
 	;   Go into pmode		;
 	;-------------------------------;
+
+EnterStage3:
 
 	cli				; clear interrupts
 	mov	eax, cr0		; set bit 0 in cr0--enter pmode
 	or	eax, 1
 	mov	cr0, eax
 
-	jmp	CODE_DESC:Stage3	; far jump to fix CS.
+	jmp	CODE_DESC:Stage3	; far jump to fix CS
 
 	; Note: Do NOT re-enable interrupts! Doing so will triple fault!
 	; We will fix this in Stage 3.
@@ -91,62 +93,7 @@ main:
 ;	ENTRY POINT FOR STAGE 3
 ;******************************************************
 
-bits 32         ; Welcome to the 32 bit world!
-%define VIDEO_MEM 0xB8000
-%define COL_NUM 80
-%define ROW_NUM 25
-%define PIXEL_WIDTH 2h
-;; 2 bytes each pixel
-%define DEFAULT_ATTR 14h
-
-
-at_x db 0
-at_y db 0
-use_char db 0
-use_attr db 0
-
-printch:
-  pusha
-
-  xor eax,eax
-  xor ecx,ecx
-  mov al,[at_x]
-  mov cl,PIXEL_WIDTH
-  mul ecx  
-  ;eax is 2*x now
-  add eax, VIDEO_MEM
-  mov ecx, eax   
-  ; ecx is 2x+VIDEO_MEM
-
-  xor eax, eax
-  xor edx,edx
-  mov al, [at_y] ;
-  mov dl, COL_NUM*PIXEL_WIDTH
-  mul edx; eax is 80y*2
-  add eax, ecx ;eax is VIDEO_MEM+2x+80y
-
-  mov cl,[use_char]
-  mov dl,[use_attr]
-  mov [eax], cl
-  inc eax
-  mov [eax], dl
-
-  popa
-  ret
-
-print_test:
-  pusha
-
-  mov cl, [use_char]
-  xor eax,eax
-  mov al, [at_x]
-  add eax, VIDEO_MEM
-  ;mov byte [0xb8000], 'A'
-  mov byte [eax],  cl
-  mov byte [0xb8001], 14h
-
-  popa
-  ret
+bits 32
 
 Stage3:
 
@@ -160,14 +107,21 @@ Stage3:
 	mov		es, ax
 	mov		esp, 90000h		; stack begins from 90000h
 
+	;---------------------------------------;
+	;   Clear screen and print success	;
+	;---------------------------------------;
 
-  mov BYTE [at_x], 1
-  mov BYTE [at_y], 1
-  mov BYTE [use_char], 'A'
-  mov BYTE [use_attr], DEFAULT_ATTR
-  call printch
-  ;;mov BYTE [use_char], 'A'
-  ;;call print_test
+	call		ClrScr32
+	mov		ebx, msg
+	call		Puts32
+
+	;---------------------------------------;
+	;   Stop execution			;
+	;---------------------------------------;
 
 	cli
 	hlt
+
+msg db  0x0A, 0x0A, 0x0A, "               <[ OS Development Series Tutorial 10 ]>"
+    db  0x0A, 0x0A,             "           Basic 32 bit graphics demo in Assembly Language", 0
+
